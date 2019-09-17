e55c751086c5791d9814c46d52dffd489fee06dd
jtf <j.t.1st@outlook.com>
2019-09-17 01:52:45 +0100

    added config file

---
+++ config/secret.go
@@ -0,0 +1,12 @@
+package config
+
+//TODO: Update DiscordToken with Original V1 Token
+
+//DiscordToken - Discord Token from Discord Developer Panel
+const DiscordToken string = ""
+
+//MongoURL - MongoDB URL location for the database
+const MongoURL string = ""
+
+//AESKey - AES Encryption / Decryption Key (Needs to be 32 characters long)
+// var AESKey = []byte("")
